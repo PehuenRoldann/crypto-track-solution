643b08d5cf26467d4f68f4b313e4c65feb94ff0f
Pehuén Roldán <roldanpehuen@gmail.com>
2024-04-02 22:58:37 -0300

    Changed CryptoCard Style

--- CryptoTrackApp/src/view/Components/CryptoCard.cs
+++ CryptoTrackApp/src/view/Components/CryptoCard.cs
@@ -30,11 +30,23 @@ namespace CryptoTrackApp.src.view.Components
             
             var name_label = new Gtk.Label(pCryptoName);
 
-            if (this.AlreadyFollow) { this._followButton = new Gtk.Button("Unfollow"); }
-            else {this._followButton = new Gtk.Button("Follow");}
+            if (this.AlreadyFollow)
+            {
+                this._followButton = new Gtk.Button("Unfollow");
+                this._followButton.StyleContext.AddClass("unfollow-button");
+            }
+            else
+            {
+                this._followButton = new Gtk.Button("Follow");
+                this._followButton.StyleContext.AddClass("follow-button");
+            }
             this._followButton.ButtonReleaseEvent += FollowButtonReleased;
 
+            this._followButton.StyleContext.AddClass("card-button");
+
             var info_btn = new Gtk.Button("+Info");
+            info_btn.StyleContext.AddClass("card-button");
+            info_btn.StyleContext.AddClass("info-button");
 
             Gdk.Pixbuf pixbuf;
             // Cargar la imagen desde un archivo
@@ -51,7 +63,7 @@ namespace CryptoTrackApp.src.view.Components
 
             // Crear una nueva imagen con el Pixbuf escalado
             var image = new Image(scaledPixbuf);
-            
+            image.StyleContext.AddClass("card-image");
             // Añadir la imagen al contenedor vertical
             this.PackStart(name_label, false, false, 0);
             this.PackStart(image, false, false, 0);
@@ -66,11 +78,21 @@ namespace CryptoTrackApp.src.view.Components
             Console.WriteLine($"Follow button from {this.CryptoId} pressed.");
             try
             {
-                await Task.Run(() => {
-                    this._subService.AddSubscriptionAsync(ViewManager.GetInstance().UserId, this.CryptoId);
-                });
-                this._followButton.Label = "Following";
-                this._followButton.CanFocus = false;
+                if (this._followButton.Label == "Follow")
+                {
+                    await Task.Run(() => {
+                        this._subService.AddSubscriptionAsync(ViewManager.GetInstance().UserId,
+                        this.CryptoId);
+                    });
+                    this._followButton.Label = "Unfollow";
+                    // this._followButton.CanFocus = false;
+                }
+                else
+                {
+                    Console.WriteLine("Tried to unfollow but the feature isn't implemented");
+                    // this._followButton.Label = "Follow";
+                    // this._followButton.CanFocus = false;
+                }
             }
             catch (Exception error)
             {
