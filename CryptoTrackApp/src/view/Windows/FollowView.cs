90426cc14a4b1059a00ebcd2ec5f017b77cb14db
Pehuen <roldanpehuen@gmail.com>
2024-08-27 12:28:14 -0300

    Added load more button to follow view

--- CryptoTrackApp/src/view/Windows/FollowView.cs
+++ CryptoTrackApp/src/view/Windows/FollowView.cs
@@ -13,10 +13,14 @@ namespace CryptoTrackApp.src.view.Windows
         [UI] private Box _mainContainer;
         [UI] private Box _searchContainer;
         [UI] private FlowBox _flowBox;
+        [UI] private Button _loadMoreBtn;
 
         private List<string> Following {get; set;}
         private ICurrencyServices _curService;
         private ISubscriptionServices _subService;
+
+        private int _offset = 0;
+        private int _limit = 50;
         
         public FollowView (ISubscriptionServices pSubService, ICurrencyServices pCurService) 
         : base("FollowView")
@@ -27,9 +31,10 @@ namespace CryptoTrackApp.src.view.Windows
             this.CSS_PATH_DARK = "./src/css/follow_view.css";
 	        this.CSS_PATH_LIGHT = "";
             this.SetStyle("dark");
-            this._mainContainer.StyleContext.AddClass("main-container");
-            this._flowBox.StyleContext.AddClass("flow-box");
-            
+            this._mainContainer!.StyleContext.AddClass("main-container");
+            this._flowBox!.StyleContext.AddClass("flow-box");
+            this._loadMoreBtn!.Hide();
+            this._loadMoreBtn.ButtonReleaseEvent += OnLoadMoreBtnRelease;
             this.LoadFlowBox();
             this.ShowAll(); 
             
@@ -48,9 +53,10 @@ namespace CryptoTrackApp.src.view.Windows
 
         private async void LoadFlowBox()
         {
+            this._loadMoreBtn.Hide();
             this.Following = await this._subService.GetFollowedCryptosIdsAsync(ViewManager.GetInstance().UserId);
-            IDictionary<string, string> [] cryptos = await this._curService.GetCurrencies(offset: 0,
-                                                                                         limit: 50);
+            IDictionary<string, string> [] cryptos = await this._curService.GetCurrencies(offset: this._offset,
+                                                                                         limit: this._limit);
             foreach (IDictionary<string, string> crypto in cryptos)
             {
                 var cryptoCard = new CryptoCard(this._subService, crypto["Symbol"].ToLower(),
@@ -58,7 +64,19 @@ namespace CryptoTrackApp.src.view.Windows
                 cryptoCard.StyleContext.AddClass("crypto-card");
                 this._flowBox.Add(cryptoCard);
             }
+            //this._loadMoreBtn = new Gtk.Button();
+            //this._loadMoreBtn.Label = "Load More Currencies";
+            // this._loadMoreBtn.Visible = true;
+            //this._flowBox.Add(this._loadMoreBtn);
+            this._loadMoreBtn.Show();
+        }
 
+        private void OnLoadMoreBtnRelease (object sender, ButtonReleaseEventArgs args)
+        {
+            Console.WriteLine("Hola!");
+            this._offset = this._limit;
+            this._limit+= 50;
+            this.LoadFlowBox();
         }
     }
 }
