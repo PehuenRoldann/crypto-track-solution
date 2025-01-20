28aa5f7c68221a0b20f0915111d540fd9d7ec888
Pehuen Roldan <roldanpehuen@gmail.com>
2025-01-20 20:19:32 -0300

    feature: Personalización de plot, cabios en colores de background

--- CryptoTrackApp/src/services/PlotterService.cs
+++ CryptoTrackApp/src/services/PlotterService.cs
@@ -22,6 +22,8 @@ namespace CryptoTrackApp.src.services
 
         private string FONT_NAME = "Arial";
 
+        private const string PLOT_BG_COLOR_HEX = "#2A2E32";
+
 
 
         public async Task<string> GetCandlesPlot(List<(DateTime, double)> history, int width = 700, int height = 300, string title = "Candle") {
@@ -67,13 +69,17 @@ namespace CryptoTrackApp.src.services
                 }
 
                 Plot plot = new();
+                // plot.Add.Palette = new SP.Palettes.Penumbra();
 
                  
                 plot.Add.Candlestick(prices);
                 plot.Axes.DateTimeTicksBottom();
+
+                // Estilizar plot
                 plot.Font.Set(ScottPlot.Fonts.Serif);
+                plot.FigureBackground.Color = SP.Color.FromHex(PLOT_BG_COLOR_HEX);
+                plot.Axes.Color(SP.Color.FromHex("#d7d7d7"));
                 plot.YLabel("Value in USD");
-                plot.Title(title, 16);
 
                 // Definir el path donde guardar el plot
                 string plotDirectory = System.IO.Path.Combine(this._basePath, "plots");
@@ -107,168 +113,3 @@ namespace CryptoTrackApp.src.services
              
     }
 }
-
-
-/// <summary>
-        /// Generates a image with the plotbox.
-        /// </summary>
-        /// <param name="pCurrencyId"></param>
-        /// <returns>Path to the plotbox generated image</returns>
-        /* public async Task<string> GetBoxPlot (Dictionary<DateTime, List<double>> valuesPerMonth, int width = 700, int height = 300) 
-        {
-
-
-            var datesArr = valuesPerMonth.Keys.ToArray();
-
-            SP.Plot plot = new ();
-            double? minValue = null;
-            double? maxValue = null;
-            
-
-            foreach(var keyDate in valuesPerMonth.Keys) {
-
-                var box = AddBox(valuesPerMonth[keyDate], keyDate, plot);
-
-                if (minValue == null || minValue > box.BoxMin) {
-                    minValue = box.BoxMin;
-                }
-
-                if (maxValue == null || maxValue < box.BoxMax) {
-                    maxValue = box.BoxMax;
-                }
-
-                
-
-                //boxes.Add(box);
-                // plot.Add.Box(box);
-                // box.FillStyle.Color = Color.FromHex("#C68FE6");
-                
-            }
-            
-            // plot.Add.Boxes(boxes);
-            plot.Axes.SetLimits(
-                datesArr[0].ToOADate(),
-                datesArr[^1].ToOADate(),
-                minValue,
-                maxValue
-            );
-
-            var dtAx = plot.Axes.DateTimeTicksBottom();
-
-            dtAx.TickGenerator = new DateTimeFixedInterval (
-                new Month(), 1,
-                new Month(), 1,
-                dt => new DateTime(dt.Year, dt.Month, 1)
-            );
-            
-            plot.RenderManager.RenderStarting += (s, e) =>
-            {
-                Tick[] ticks = plot.Axes.Bottom.TickGenerator.Ticks;
-                for (int i = 0; i < ticks.Length; i++)
-                {
-                    DateTime dt = DateTime.FromOADate(ticks[i].Position);
-                    string label = $"{dt:MMM} {dt:yyyy}";
-                    
-                    ticks[i] = new Tick(ticks[i].Position, label);
-                }
-            };
-            
-
-            // Definir el path donde guardar el plot
-            string plotDirectory = System.IO.Path.Combine(this._basePath, "plots");
-
-            // Verificar si el directorio existe y crearlo si no es así
-            if (!System.IO.Directory.Exists(plotDirectory))
-            {
-                System.IO.Directory.CreateDirectory(plotDirectory);
-            }
-
-            string pathToPlot = System.IO.Path
-            .Combine(plotDirectory, "boxplot.png");
-            plot.SavePng(pathToPlot, width, height);
-
-            
-
-            //return plot;
-            return pathToPlot;
-                        
-        }
-
-        private SP.Box AddBox(List<double> pValuesList, DateTime pDate, SP.Plot pPlot)
-        {
-            double[] valuesArr = pValuesList.ToArray();
-            
-            double change = (valuesArr[^1] - valuesArr[0]) / valuesArr[0] * 100;
-            
-            QuickSort(valuesArr, 0, valuesArr.Length - 1);
-
-            SP.Color color = new();
-
-            if (change >= 0) { color = SP.Color.FromHex("#38E54D"); }
-            else { color = SP.Color.FromHex("#F94C10"); }
-
-            SP.Box box = new()
-            {
-                Position = pDate.ToOADate(),
-                WhiskerMax = valuesArr[^1],
-                BoxMax = CalcQuertile(3, valuesArr),
-                BoxMiddle = CalcQuertile(2, valuesArr),
-                BoxMin = CalcQuertile(1, valuesArr),
-                WhiskerMin = valuesArr[0],
-                Width = 5,
-                FillColor = color
-            };
-
-            pPlot.Add.Box(box);
-            // If changed before added, will have a random color
-            box.FillStyle.Color = color; 
-            return box;
-        }
-
-        private double CalcQuertile(int quartile, double[] values) {
-            
-            double n = values.Length;
-
-            double q = quartile*(n+1)/4;
-
-            return values[(int)Math.Round(q)];
-        }
-
-
-        private void QuickSort(double[] arr, int low, int high) {
-
-            if ( low < high ) {
-
-                int pi = Partition(arr, low, high);
-
-                QuickSort(arr, low, pi-1);
-                QuickSort(arr, pi+1, high);
-            }
-
-        }
-
-        private int Partition (double[] arr, int low, int high) {
-
-            double pivot = arr[high]; // Pivot value
-
-            int pivotActualIndex = low; //Index to put the pivot, also cant of elements before the pivot
-
-            for (int j = low; j <= high - 1; j++) {
-
-                if (arr[j] < pivot) {
-
-                    pivotActualIndex++;
-                    SwapValues(arr, pivotActualIndex-1, j);
-                }
-            }
-
-            SwapValues(arr, pivotActualIndex, high);
-            return pivotActualIndex;
-        }
-
-        private void SwapValues (double[] arr, int low, int high) {
-
-            double aux = arr[high];
-            arr[high] = arr[low];
-            arr[low] = aux;
-        } */
